196d2c7554bf256536b361c08c46a7b6dbc3211e
Pearu Pung <pungpearu@gmail.com>
2019-10-12 17:13:17 +0300

    LEG_01 MenuItem implemented

--- FourConnectTestSolution/FourConnectCore/MenuItem.cs
+++ FourConnectTestSolution/FourConnectCore/MenuItem.cs
@@ -1,7 +1,39 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace FourConnectCore
 {
     public class MenuItem
     {
-        
+        private string _title;
+
+        public string Title
+        {
+            get => _title;
+            set => _title = Validate(value, 1, 100, false);
+        }
+
+        private static string Validate(string item, int minLength, int maxLength, bool toUpper)
+        {
+            item = item.Trim();
+            if (toUpper)
+            {
+                item = item.ToUpper();
+            }
+
+            if (item.Length < minLength || item.Length > maxLength)
+            {
+                throw new ArgumentException($"String length {item.Length} " +
+                                            $"of {item} is invalid, " +
+                                            $"must be in range {minLength}-{maxLength}");
+            }
+
+            return item;
+        }
+
+        public override string ToString()
+        {
+            return Title;
+        }
     }
 }
