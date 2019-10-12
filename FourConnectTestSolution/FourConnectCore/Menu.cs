ec251733f02f7aaa3690ce9d93b28b21162051cf
Pearu Pung <pungpearu@gmail.com>
2019-10-12 16:57:22 +0300

    LEG_01 Initial menu

--- FourConnectTestSolution/FourConnectCore/Menu.cs
+++ FourConnectTestSolution/FourConnectCore/Menu.cs
@@ -1,7 +1,22 @@
+using System.Collections.Generic;
+
 namespace FourConnectCore
 {
     public class Menu
     {
+        private int _menuLevel;
+
+        private const string MenuCommandExit = "X";
+        private const string MenuCommandReturnToPrevious = "P";
+        private const string MenuCommandReturnToMain = "M";
+
+        private Dictionary<string, MenuItem> _menuItemsDictionary = new Dictionary<string, MenuItem>();
+
+        public Menu(int menuLevel = 0)
+        {
+            _menuLevel = menuLevel;
+        }
         
+        public string Title { get; set; }
     }
 }
