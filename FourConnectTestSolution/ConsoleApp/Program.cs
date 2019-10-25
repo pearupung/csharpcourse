ff167a00b9ea364f9f0d07b117ca0c6af41e933e
Pearu Pung <pungpearu@gmail.com>
2019-10-25 18:34:34 +0300

    CON4-001 Implement stack menu MVP in main

--- FourConnectTestSolution/ConsoleApp/Program.cs
+++ FourConnectTestSolution/ConsoleApp/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Transactions;
+using FourConnectCore.MenuItems;
 
 namespace FourConnectCore
 {
@@ -11,20 +12,47 @@ namespace FourConnectCore
         static void Main(string[] args)
         {
             var menuView = new MenuView();
-            RunMenu(menuView);
-            menuView.SetStartMenu(_menu);
-
+            menuView.SetStartMenu(new MainMenu());
+            var failedAction = MenuAction.Chill;
             do
             {
+                Console.Clear();
+                Console.WriteLine(failedAction == 0 ? "" : failedAction.ToString());
+                failedAction = 0;
                 Console.WriteLine(menuView);
-                
                 var command = Console.ReadLine()?.Trim().ToUpper() ?? "";
                 if (menuView.MenuItems.ContainsKey(command))
                 {
                     var menuItem = menuView.MenuItems[command];
-                    menuItem.CommandToExecute();
+                    var action = menuView.PickMenuItem(menuItem);
+                    switch (action)
+                    {
+                        case MenuAction.Exit:
+                            menuView.Exit();
+                            break;
+                        case MenuAction.LeaveMenu:
+                            menuView.LeaveMenu();
+                            break;
+                        case MenuAction.GoToMainMenu:
+                            menuView.GoToMainMenu();
+                            break;
+                        case MenuAction.GoToGameMenu:
+                            menuView.GoToMenu(new GameMenu());
+                            break;
+                        case MenuAction.GoToGamePrepMenu:
+                            menuView.GoToMenu(new GamePrepMenu());
+                            break;
+                        case MenuAction.GoToSettingsMenu:
+                            menuView.GoToMenu(new SettingsMenu());
+                            break;
+                        case MenuAction.GoToLoadGameMenu:
+                            menuView.GoToMenu(new LoadGameMenu());
+                            break;
+                        default:
+                            failedAction = action;
+                            break;
+                    }
                 }
-                
 
             } while (menuView.MenuStackSize > 0);
         }
@@ -94,7 +122,8 @@ namespace FourConnectCore
                             {
                                 menuView.GoToMenu(gameMenu);
                                 return "";
-                            }
+                            },
+                            ActionToTake = MenuAction.GoToGameMenu
                         }
                     },
                     {
