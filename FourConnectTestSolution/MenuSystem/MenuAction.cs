2380ecb82c2f226405e1c7680fc547221a7ca744
Pearu Pung <pungpearu@gmail.com>
2019-10-25 18:28:26 +0300

    CON4-001 Add actions enum to describe all actions

---
+++ FourConnectTestSolution/MenuSystem/MenuAction.cs
@@ -0,0 +1,24 @@
+namespace FourConnectCore
+{
+    public enum MenuAction
+    {
+        Chill = 0,
+        LeaveMenu,
+        GoToMainMenu,
+        Exit,
+        StartGame,
+        GoToGameMenu,
+        GoToLoadGameMenu,
+        GoToSettingsMenu,
+        PlayAgainstTheMachine,
+        PlayAgainstALocalPlayer,
+        PlayX,
+        PlayO,
+        GoLeftOneColumn,
+        GoRightOneColumn,
+        SaveTheGame,
+        Confirm,
+        Cancel,
+        GoToGamePrepMenu
+    }
+}
